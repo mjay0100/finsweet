e95e23c8c0a6ddd8004ca1d17c95c9e02526b607
mustapha jay <mustaphajay2@gmail.com>
2022-07-02 23:59:35 +0100

    added first part of about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section>
+      <div className="flex gap-5">
+        <div className="mt-[2rem] py-[5.5rem] pl-[5rem]">
+          <p className="uppercase mb-6">Know about us</p>
+          <h1 className="text-3xl mb-6">We help nature smile and survive </h1>
+          <p className="mb-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+            neque porro ipsa laboriosam earum provident, veniam quae, dolores,
+            ab ipsum id officiis error dolore! Provident molestiae mollitia
+            accusamus voluptatibus quibusdam?
+          </p>
+          <p className="mb-[2rem]">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis
+            a deleniti odit nobis, magnam itaque minus asperiores, sint pariatur
+            vitae, quo recusandae quasi optio sequi id iusto doloremque?
+            Doloremque, corrupti?
+          </p>
+          <button className="bg-green-400 hover:bg-green-500 transition-all duration-300 py-2 px-4 rounded-md text-white">
+            Learn More
+          </button>
+        </div>
+        <img
+          src="https://images.unsplash.com/photo-1605713288610-00c1c630ca1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNoaWxkcmVufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+      </div>
+      <div className="flex items-center gap-[1rem] mt-[4rem] justify-center">
+        <p className="text-lg">Our supporters</p>
+        <div className="w-[80%] h-[1px] bg-slate-400"></div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
