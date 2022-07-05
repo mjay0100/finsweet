985e82fe98240ee613ec7259729418d72e9fcdb1
mustapha jay <mustaphajay2@gmail.com>
2022-07-05 11:53:21 +0100

    added logos using logoipsum

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import logo from "../public/images/logo.svg";
+import logo2 from "../public/images/logo2.svg";
+import logo3 from "../public/images/logo3.svg";
 
 const About = () => {
   return (
-    <section>
+    <section className="mx-[3rem]">
       <div className="flex gap-5">
         <div className="mt-[2rem] py-[5.5rem] pl-[5rem]">
           <p className="uppercase mb-6">Know about us</p>
@@ -24,13 +27,22 @@ const About = () => {
           </button>
         </div>
         <img
+          className="h-[70vh] px-[5rem] my-[4rem] rounded-md"
           src="https://images.unsplash.com/photo-1605713288610-00c1c630ca1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNoaWxkcmVufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
           alt=""
         />
       </div>
-      <div className="flex items-center gap-[1rem] mt-[4rem] justify-center">
+      <div className="flex items-center gap-[1.5rem] justify-center mb-[1.5rem]">
         <p className="text-lg">Our supporters</p>
-        <div className="w-[80%] h-[1px] bg-slate-400"></div>
+        <div className="w-[77%] h-[1px] bg-slate-400"></div>
+      </div>
+      <div className="flex text-lg text-[#ccc] items-center justify-evenly mb-[3.5rem]">
+        <img src={logo2} alt="" />
+        <img src={logo} alt="" />
+        <img src={logo3} alt="" />
+        <img src={logo2} alt="" />
+        <img src={logo} alt="" />
+        <img src={logo3} alt="" />
       </div>
     </section>
   );
