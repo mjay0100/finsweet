5c8494df9bb17581d9f0fa03284814625b04bbbc
mustapha jay <mustaphajay2@gmail.com>
2022-07-05 21:18:26 +0100

    added work component content

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,53 @@
-import React from "react";
-
 const Works = () => {
-  return <div>Works</div>;
+  return (
+    <section className="mx-[3rem] mb-[4rem]">
+      <div className="mt-[2rem] py-[5.5rem] pl-[5rem]">
+        <p className="uppercase mb-6">what we have done</p>
+        <h1 className="text-[3rem] mb-6 font-bold">
+          we are creating sustainable <br /> society, for everyone and <br />{" "}
+          forever.
+        </h1>
+      </div>
+      <div className="flex gap-5">
+        <div className="bg-1  rounded-lg py-[4rem] px-[2rem] w-[33%]">
+          <div className="mb-6 text-white p-[2rem]  ">
+            <h1 className="text-2xl">Lorem ipsum dolor sit</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad,
+              libero?
+            </p>
+          </div>
+          <button className="bg-white p-3 mb-4 ml-[2rem] rounded-md">
+            learn more
+          </button>
+        </div>
+        <div className="bg-2  rounded-lg py-[4rem] px-[2rem] w-[33%]">
+          <div className="mb-6 text-white p-[2rem]  ">
+            <h1 className="text-2xl">Lorem ipsum dolor sit</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad,
+              libero?
+            </p>
+          </div>
+          <button className="bg-white p-3 mb-4 ml-[2rem] rounded-md">
+            learn more
+          </button>
+        </div>
+        <div className="bg-3  rounded-lg py-[4rem] px-[2rem] w-[33%]">
+          <div className="mb-6 text-white p-[2rem]  ">
+            <h1 className="text-2xl">Lorem ipsum dolor sit</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad,
+              libero?
+            </p>
+          </div>
+          <button className="bg-white p-3 mb-4 ml-[2rem] rounded-md">
+            learn more
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Works;
