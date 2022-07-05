8d5698b416cb31c44e215a6dfd3fd7af5948a678
mustapha jay <mustaphajay2@gmail.com>
2022-07-05 11:48:27 +0100

    add work component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,13 @@
 import Cover from "./Cover";
 import About from "./About";
+import Work from "./Work";
 
 const Content = () => {
   return (
     <div>
       <Cover />
       <About />
+      <Work />
     </div>
   );
 };
