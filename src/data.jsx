e42241798bb9a7558eed93b1a65997c4ce0b9777
mustapha jay <mustaphajay2@gmail.com>
2022-07-05 14:57:48 +0100

    initial commit added work data

---
+++ src/data.jsx
@@ -0,0 +1,28 @@
+import { BiBuildings } from "react-icons/bi";
+
+export const workData = [
+  {
+    id: 1,
+    icon: <BiBuildings />,
+    title: "Build healthy cities",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiqu neque porro",
+  },
+  {
+    id: 2,
+    icon: <BiBuildings />,
+    title: "Build healthy cities",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiqu neque porro",
+  },
+  {
+    id: 3,
+    icon: <BiBuildings />,
+    title: "Build healthy cities",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiqu neque porro",
+  },
+  {
+    id: 4,
+    icon: <BiBuildings />,
+    title: "Build healthy cities",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiqu neque porro",
+  },
+];
