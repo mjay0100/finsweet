b4053dcff433ac3b9daeedc6743476345fd77e7f
mustapha jay <mustaphajay2@gmail.com>
2022-07-06 10:44:04 +0100

    added component content

--- src/components/Contribution.jsx
+++ src/components/Contribution.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-
 const Contribution = () => {
-  return <div>Contribution</div>;
+  return (
+    <section className="contribution-bg mx-[6rem] rounded-lg mb-[4rem] text-white">
+      <div className="mt-[5rem] py-[5.5rem] ">
+        <h1 className="text-[3rem] mb-6 text-center font-bold">
+          You can contribute to make <br /> the environment greener!
+        </h1>
+        <div className="flex gap-4 justify-center">
+          <button className="bg-green-500 py-3 px-5 rounded-md">
+            join as a volunteer
+          </button>
+          <button className="text-black bg-white py-3 px-5 rounded-md">Donate</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contribution;
