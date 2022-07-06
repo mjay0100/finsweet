6da73cf29f1cf12e2ee2e1ea84b8bccc53cdbd8d
mustapha jay <mustaphajay2@gmail.com>
2022-07-06 12:20:49 +0100

    added News component data

--- src/data.jsx
+++ src/data.jsx
@@ -1,5 +1,5 @@
 import { BiBuildings } from "react-icons/bi";
-import {GiBurningTree, GiSittingDog} from 'react-icons/gi'
+import { GiBurningTree, GiSittingDog } from "react-icons/gi";
 import { FaWater } from "react-icons/fa";
 
 export const workData = [
@@ -26,5 +26,32 @@ export const workData = [
     icon: <GiSittingDog />,
     title: "animal safety",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similiqu neque porro",
+  },
+];
+
+export const newsData = [
+  {
+    id: 1,
+    img: "https://media.istockphoto.com/photos/thoroughbred-horses-grazing-at-sunset-in-a-field-picture-id1349772438?b=1&k=20&m=1349772438&s=170667a&w=0&h=dzJsbcThM9uMYOZwXzZcvBl6jz6_PY38YSzwYfWqmtU=",
+    title: "Don't destroy greenery and don't spoil scenery",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, libero?",
+  },
+  {
+    id: 2,
+    img: "https://media.istockphoto.com/photos/sustainable-power-is-the-future-picture-id1316576079?b=1&k=20&m=1316576079&s=170667a&w=0&h=xUlSLnxYUJDCd5WTb7VDbxgJq9i2TJ52JqvEy5VDKzc=",
+    title: "Top 10 facts about wind farms you didn/'t know",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, libero?",
+  },
+  {
+    id: 3,
+    img: "https://media.istockphoto.com/photos/aerial-view-of-the-mountain-road-in-a-green-forest-picture-id1330597560?b=1&k=20&m=1330597560&s=170667a&w=0&h=6uL4OGuNad2sKmVrOJzyp3eK_qUDp1DnggPo1jFTDl4=",
+    title: "Is climate change happening faster than expected?",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, libero?",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    title: "Our goal is make water available for everyone",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, libero?",
   },
 ];
