1a4a653069c48527612df4ac16d7ea5697607b4a
mustapha jay <mustaphajay2@gmail.com>
2022-07-06 12:23:39 +0100

    added content to component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { newsData } from "../data";
 
 const News = () => {
-  return <div>News</div>;
+  return (
+    <section className=" mx-[6rem] rounded-lg mb-[4rem]">
+      <div className="mt-[6rem] mb-[4rem] items-center flex gap-2 ">
+        <h1 className="text-[2rem] font-bold">Read Our News</h1>
+        <div className="w-[77%] h-[1px] bg-slate-400"></div>
+      </div>
+      <div className="flex justify-between gap-5">
+        {newsData.map((data) => {
+          const { id, img, title, text } = data;
+          return (
+            <div key={id}>
+              <img className="mb-3 h-[180px]" src={img} alt="" />
+              <h1 className="text-[1.3rem] font-semibold mb-3">{title}</h1>
+              <p>{text}</p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default News;
