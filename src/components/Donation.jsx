aa866dba5c8c01fd513a287bbbc0a260f762ddc0
mustapha jay <mustaphajay2@gmail.com>
2022-07-15 20:41:41 +0100

    modified styling

--- src/components/Donation.jsx
+++ src/components/Donation.jsx
@@ -20,39 +20,42 @@ const data = {
 
 const Donation = () => {
   return (
-    <section className=" mb-[4rem] p-[1.2rem] md:flex justify-between bg-black text- py-[2.5rem]">
-      <div className="mt-[2rem]  py-[5.5rem] pl-[5rem] text-white">
-        <h1 className="text-[2rem] mb-6 font-bold">
-          How we spend your <br /> donations and where it goes forever.
-        </h1>
-        <p className="text-white/60">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, libero?
-        </p>
-        <div className="grid grid-cols-2 gap-3 mt-[3rem]">
-          <div className="flex gap-4">
-            <input type="checkbox" />
-            <p>40% planting trees</p>
-          </div>
-          <div className="flex gap-4">
-            <input type="checkbox" />
-            <p>35% planting trees</p>
-          </div>
-          <div className="flex gap-4">
-            <input type="checkbox" />
-            <p>10% planting trees</p>
-          </div>
-          <div className="flex gap-4">
-            <input type="checkbox" />
-            <p>10% planting trees</p>
-          </div>
-          <div className="flex gap-4">
-            <input type="checkbox" />
-            <p>5% planting trees</p>
+    <section className=" mb-[4rem] bg-black text-white">
+      <div className="md:flex mx-auto w-[85%] items-center p-[3rem]">
+        <div className="mr-6">
+          <h1 className="text-[2rem] mb-6 font-bold">
+            How we spend your <br /> donations and where it goes forever.
+          </h1>
+          <p className="text-white/60">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A d ,
+            libero?
+          </p>
+          <div className="grid grid-cols-2 gap-3 mt-[3rem]">
+            <div className="flex gap-4">
+              <input type="checkbox" />
+              <p>40% planting trees</p>
+            </div>
+            <div className="flex gap-4">
+              <input type="checkbox" />
+              <p>35% planting trees</p>
+            </div>
+            <div className="flex gap-4">
+              <input type="checkbox" />
+              <p>10% planting trees</p>
+            </div>
+            <div className="flex gap-4">
+              <input type="checkbox" />
+              <p>10% planting trees</p>
+            </div>
+            <div className="flex gap-4">
+              <input type="checkbox" />
+              <p>5% planting trees</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="w-[50%] mx-auto md:my-auto md:mx-[4rem]">
-        <Doughnut data={data} />
+        <div className="w-[50%] my-[2rem] md:w-[35%] mx-auto">
+          <Doughnut data={data} />
+        </div>
       </div>
     </section>
   );
