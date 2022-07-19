742cdda0a1f4052d4bed0b7227bd62f431bebdf1
mustapha jay <mustaphajay2@gmail.com>
2022-07-19 11:23:20 +0100

    added service route page

--- src/router/Index.jsx
+++ src/router/Index.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Home from "../pages/Home";
 import About from "../pages/About";
-
+import Services from "../pages/Services.jsx";
 
 export default function Index() {
   return (
@@ -13,8 +13,9 @@ export default function Index() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
