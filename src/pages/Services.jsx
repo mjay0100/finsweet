0c2f196fd2f0e4c1e73c04a53cfd3746be528673
mustapha jay <mustaphajay2@gmail.com>
2022-07-19 11:22:43 +0100

    initial commit

---
+++ src/pages/Services.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat laborum
+      ab, temporibus dolorem enim consequatur atque expedita cumque odio
+      corrupti alias cupiditate incidunt exercitationem minus? Ipsa nesciunt et,
+      quos corporis animi quam fugit. Architecto nisi non consequatur.
+      Voluptatibus autem repudiandae tempora aliquid odit voluptate vitae
+      aliquam adipisci! Voluptatibus earum facere, sunt deserunt suscipit itaque
+      commodi odio voluptatum nemo fugiat. Alias ea impedit quis aliquid,
+      doloribus, enim sint reiciendis nulla qui similique iste fuga. Autem
+      consequatur sed rerum veritatis optio provident repellat qui hic! Vitae
+      aut nulla officiis provident, laborum ea perspiciatis, non maxime a at
+      dolor reprehenderit cum atque consequuntur.
+    </div>
+  );
+};
+
+export default Services;
