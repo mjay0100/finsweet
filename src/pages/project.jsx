609eb60f76d277259c6904891d0534c547fb66c9
mustapha jay <mustaphajay2@gmail.com>
2022-07-24 13:42:25 +0100

    added some content to project page

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,5 +1,68 @@
-const FProject = () => {
-  return <div>project</div>;
+import Works from "../components/Works";
+import Contribution from "../components/Contribution";
+import projectImg from "../public/images/projectImg.png";
+
+const Project = () => {
+  return (
+    <>
+      <section className="w-[85%] mx-auto md:w-[60%] mt-8">
+        <p className="text-lg font-semibold uppercase mb-4">our project</p>
+        <h1 className="font-bold text-5xl mb-8">
+          Weekly cleanliness program in the city
+        </h1>
+        <p className="mb-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          neque porro ipsa laboriosam earum provident, veniam quae, dolores, ab
+          ipsum id officiis error dolore! Provident molestiae mollitia accusamus
+          voluptatibus quibusdam? ipsum id officiis error dolore! Provident
+          molestiae mollitia accusamus voluptatibus quibusdam?
+        </p>
+        <p className="mb-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          neque porro ipsa laboriosam earum provident, veniam quae, dolores, ab
+          ipsum id officiis error dolore! Provident molestiae mollitia accusamus
+          voluptatibus quibusdam?ipsum id officiis error dolore! Provident
+          molestiae mollitia accusamus voluptatibus quibusdam?
+        </p>
+        <p className="mb-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          neque porro ipsa laboriosam earum provident, veniam quae, dolores, ab
+          ipsum id officiis error dolore! Provident molestiae mollitia accusamus
+          voluptatibus quibusdam? ipsum id officiis error dolore! Provident
+          molestiae mollitia accusamus voluptatibus quibusdam?
+        </p>
+        <img src={projectImg} alt="" />
+        <h1 className="font-bold my-8 text-3xl">Some cool heading</h1>
+        <p className="mb-5">
+          Posuere sed pulvinar enim faucibus justo, cursus. In diam facilisi at
+          sit convallis blandit blandit in. Maecenas odio orci lectus urna ante
+          consequat erat non morbi. Rhoncus ullamcorper dictum sit non semper
+          sit tellus adipiscing. Est sapien rhoncus sit vestibulum sollicitudin.
+          Tellus urna malesuada eu id tempus lorem. Est aliquam sem bibendum
+          felis interdum pharetra. Diam fermentum in lectus morbi at eget sit et
+          quisque. Semper commodo viverra donec magna egestas nibh. Condimentum
+          pellentesque auctor ornare at at tellus nunc cras eu. Velit dignissim
+          penatibus faucibus tempus. Arcu pharetra morbi bibendum et dolor
+          volutpat amet. Sed mauris amet mi sed purus vitae odio. Mi eu lectus
+          suscipit sagittis, ultrices ut dui.
+        </p>
+      </section>
+      <Works title="" header="" />
+      <Contribution />
+    </>
+  );
 };
 
 export default Project;
+          
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+        
