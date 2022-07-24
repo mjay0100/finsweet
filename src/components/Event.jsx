38d8709b658b21e7214cf8a135aef894c38b13de
mustapha jay <mustaphajay2@gmail.com>
2022-07-24 18:12:51 +0100

    added default props

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,10 +1,11 @@
 import { AiOutlineArrowRight } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
-const Event = () => {
+const Event = ({title}) => {
   return (
     <section className=" mx-[1rem] rounded-lg mb-[4rem]">
       <div className="mt-[6rem] mb-[4rem] items-center md:flex gap-2 ">
-        <h1 className="mb-3 text-[2rem] font-bold">Our Events</h1>
+        <h1 className="mb-3 text-[2rem] font-bold">{title}</h1>
         <div className="w-[77%] h-[1px] bg-slate-400"></div>
       </div>
       <div className="flex gap-5">
@@ -23,7 +24,9 @@ const Event = () => {
             </h1>
           </div>
           <span className="text-black text-[1.2rem] bg-white rounded-[50%] p-5">
-            <AiOutlineArrowRight />
+            <Link to="/event">
+              <AiOutlineArrowRight />
+            </Link>
           </span>
         </div>
         <div className="flex items-center justify-between gap-5 bg-green-300 p-8 rounded-lg w-[100%]">
@@ -39,12 +42,19 @@ const Event = () => {
             <h1 className="text-2xl font-bold">Weekly cleaning program</h1>
           </div>
           <span className="text-black text-[1.2rem] bg-white rounded-[50%] p-5">
-            <AiOutlineArrowRight />
+            <Link to="/event">
+              <AiOutlineArrowRight />
+            </Link>
           </span>
         </div>
       </div>
     </section>
   );
 };
+
+Event.defaultProps = {
+  title: "Our Events",
+};
+
 
 export default Event;
